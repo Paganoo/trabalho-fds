ea85446d52552bb59fab21840f894e9bfbb04cc9
Paganoo <victorpaganodossantos@gmail.com>
2025-05-17 14:41:37 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,3 @@
+function mostrarMensagem() {
+            document.getElementById("mensagem").innerText = "A jornada pode ser difícil, mas cada passo te leva mais perto do seu objetivo. 🚀";
+        }
